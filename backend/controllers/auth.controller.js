2e32e421ef7c5c86a5eb8b87aab21bca860a2b87
NicolasCornaglia <nicolascornaglia0@gmail.com>
2024-10-21 12:16:51 +0200

    1 - project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+export const signup = (req, res) => {
+    res.json({
+        data: "signup endpoint"
+    });
+}
+
+export const login = (req, res) => {
+    res.json({
+        data: "login endpoint"
+    });
+}   
+
+export const logout = (req, res) => {
+    res.json({
+        data: "logout endpoint"
+    });
+}
+
+
